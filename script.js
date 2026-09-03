5412e0b4031beaf1c5b1cb862ab99fa173305281
masakiui <masaki0612jp@gmail.com>
2026-09-03 17:47:03 +0900

    Add job data structure in script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+GUARDIAN βテスト版
+求人データ
+*/
+
+const jobs = [
+  {
+    id: 1,
+    facility: "ひかりこども園",
+    jobType: "保育士",
+employment: "正社員",
+    salary: "月給　21,000円～",
+    location: ”岡山市南区”,
+    workingHours: "7:00~19:00の間でシフト制",
+
+    
